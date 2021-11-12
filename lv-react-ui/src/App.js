e7c51ca1f0ee87737bdc56f1bf28be82a9bf5b7c
Samuel Ventura <samuel.ventura@yeico.com>
2021-11-12 14:31:36 -0600

    Hiram training

---
+++ lv-react-ui/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function clickHandler() {
+  console.log("click")
+  fetch('/ping')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log("error:", error))
+}
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={clickHandler}>Ping</button>
+    </div>
+  );
+}
+
+export default App;
