2777f8ce566fce965c0d721e36415dc50b830a9b
Samuel Ventura <samuel.ventura@yeico.com>
2021-12-07 01:43:46 -0600

    cs dn6 sp tryout

---
+++ cs-serial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Ports;
+
+namespace MyApp
+{
+    //dotnet run #wont work
+    //dotnet build
+    //dotnet bin/Debug/net6.0/cs-serial.dll
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            var sp = new SerialPort();
+            sp.PortName = "/dev/ttyUSB0";
+            sp.ReadTimeout = 800;
+            sp.Open();
+            while (true)
+            {
+                sp.Write(new byte[] { 1, 5, 16, 9, 255, 0, 88, 248 }, 0, 8);
+                Read(sp, 8);
+                System.Threading.Thread.Sleep(100);
+                sp.Write(new byte[] { 1, 5, 16, 9, 0, 0, 25, 8 }, 0, 8);
+                Read(sp, 8);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private static void Read(SerialPort sp, int nn)
+        {
+            for (var i = 0; i < nn; i++)
+            {
+                // var b = sp.ReadByte();
+                // System.Console.WriteLine("<{0} {1}", i, b);
+            }
+        }
+    }
+}
