8970d9636f148e8c9d6d3bd37fb4b498529ca5cf
Samuel Ventura <samuel.ventura@yeico.com>
2021-12-17 17:32:39 -0600

    basic initial polynomial parser

--- go-parser/parser.go
+++ go-parser/parser.go
@@ -6,11 +6,113 @@ import (
 	"strings"
 )
 
-func Evaluate(expr string, value float64) float64 {
-	parts := strings.Split(expr, "^")
-	expo, err := strconv.ParseFloat(parts[1], 64)
-	if err != nil {
-		panic(err)
+type node struct {
+	expr  string
+	left  *node
+	right *node
+}
+
+func parse(expr string) *node {
+	n := &node{}
+	for i, c := range expr {
+		if i == 0 {
+			continue
+		}
+		switch c {
+		case '+':
+			n.expr = "+"
+			n.left = parsei(expr[:i])
+			n.right = parse(expr[i:])
+			return n
+		case '-':
+			n.expr = "+"
+			n.left = parsei(expr[:i])
+			n.right = parse(expr[i:])
+			return n
+		}
 	}
-	return math.Pow(value, expo)
+	return parsei(expr)
+}
+
+func parsei(expr string) *node {
+	n := &node{}
+	s := 0
+	for i, c := range expr {
+		if i == 0 {
+			switch c {
+			case '+':
+				s = 1
+				continue
+			case '-':
+				n.expr = "*"
+				n.left = &node{expr: "-1"}
+				n.right = parsei(expr[i+1:])
+				return n
+			}
+		}
+		switch c {
+		case '*':
+			n.expr = string(c)
+			n.left = parsei(expr[s:i])
+			n.right = parsei(expr[i+1:])
+			return n
+		case '^':
+			n.expr = string(c)
+			n.left = parsei(expr[s:i])
+			n.right = parsei(expr[i+1:])
+			return n
+		}
+	}
+	n.expr = expr
+	return n
+}
+
+func evald(expr string, value float64) float64 {
+	root := parse(expr)
+	return eval(root, value)
+}
+
+func eval(n *node, value float64) float64 {
+	if n.left != nil && n.right != nil {
+		left := eval(n.left, value)
+		right := eval(n.right, value)
+		switch n.expr {
+		case "+":
+			return left + right
+		case "*":
+			return left * right
+		case "^":
+			return math.Pow(left, right)
+		default:
+			panic("Unknown operation")
+		}
+	}
+	switch n.expr {
+	case "x":
+		return value
+	case "+x":
+		return value
+	default:
+		val, err := strconv.ParseFloat(n.expr, 64)
+		if err != nil {
+			panic(err)
+		}
+		return val
+	}
+}
+
+func evals(n *node) string {
+	var b strings.Builder
+	if n.left != nil && n.right != nil {
+		b.WriteString("(")
+		b.WriteString(n.expr)
+		b.WriteString(" ")
+		b.WriteString(evals(n.left))
+		b.WriteString(" ")
+		b.WriteString(evals(n.right))
+		b.WriteString(")")
+		return b.String()
+	}
+	b.WriteString(n.expr)
+	return b.String()
 }
--- go-parser/parser_test.go
+++ go-parser/parser_test.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 //go test
+
 func TestParser(t *testing.T) {
-	assert.Equal(t, 25.0, Evaluate("x^2", 5))
-	assert.Equal(t, 36.0, Evaluate("x^2", 6))
-	assert.Equal(t, 8.0, Evaluate("x^3", 2))
+	root := parse("x^2+1")
+	assert.Equal(t, "+", root.expr)
+	assert.Equal(t, "^", root.left.expr)
+	assert.Equal(t, "x", root.left.left.expr)
+	assert.Equal(t, "2", root.left.right.expr)
+	assert.Equal(t, "+1", root.right.expr)
+}
+
+func TestEvaluator(t *testing.T) {
+	assert.Equal(t, 25.0, evald("x^2", 5))
+	assert.Equal(t, 36.0, evald("x^2", 6))
+	assert.Equal(t, 8.0, evald("x^3", 2))
+	ast := parse("+x^2+2*x+1")
+	fmt.Println(evals(ast))
+	assert.Equal(t, 9.0, eval(ast, 2))
+	assert.Equal(t, 16.0, eval(ast, 3))
+	ast2 := parse("+x^4-x^2-1")
+	fmt.Println(evals(ast2))
+	assert.Equal(t, 11.0, eval(ast2, 2))
 }
